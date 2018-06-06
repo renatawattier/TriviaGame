d637c20af8ecc4795fa5b3cd00335a7766814786
coxipozina <coxipozina@gmail.com>
2018-06-05 17:56:20 -0700

    working on final display

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,24 @@
 var questions = [{
-    ques: "WHICH COUNTRY  HAS WON THE MOST FIFA WORLD CUPS? ?",
+    ques: "WHICH COUNTRY  HAS WON THE MOST FIFA WORLD CUPS?",
     ans: ["BRASIL", "ITALY", "GERMANY", "ARGENTINA"],
     name: "mostWins",
-    correct: "Brasil",
+    correct: "BRASIL",
     divClass: ".mostWins"
 },
 {
-    ques: "HOW MANY FIFA WORLD CUPS HAVE BRAZIL WON??",
+    ques: "HOW MANY FIFA WORLD CUPS HAVE BRAZIL WON?",
     ans: ["6", "5", "4", "7"],
     name: "wins",
     correct: "5",
     divClass: ".wins"
 },
 {
-    ques: "FOR HOW MANY TIMES BRAZIL HAVE REACHED FIFA WORLD CUP FINALE??",
+    ques: "FOR HOW MANY TIMES BRAZIL HAVE REACHED FIFA WORLD CUP FINALE?",
     ans: ["10", "6", "7", "8"],
     name: "finale",
     correct: "7",
-    divClass: "finale"
+    divClass: ".finale"
+    
 },
 {
     ques: "NAME THE ONLY PLAYER TO HAVE WON THE WORLD CUP THREE TIMES?",
@@ -35,112 +36,79 @@ var questions = [{
 },
 {
     ques: "NAME THE ONLY PERSON THAT HAS WON A WORLD CUP MEDAL 4 TIMES?",
-    ans: ["GerdMuller - Germany", "PaoloMaldini - Italy", "Zagallo - Brazil", "Hidefumi Hommyo"],
+    ans: ["GerdMuller - Germany", "PaoloMaldini - Italy", "Zagallo - Brazil", "Pele - Brasil"],
     name: "medals",
     correct: "Zagallo - Brazil",
     divClass: "medals"
-},
+}]
 
-] // end questions object
 
-var labels = ["first", "second", "third", "forth"];
+var wrongAnswers = 0
+var correctAnswers = 0
 
-// click to start then display quesions
-var startGame = $("#start-btn").on('click', function() {
-$(this).parent().hide();
-$('.container').show();
-countdown(60);
-questionDisplay();
+
+$(".hiddenShow").hide()
+  
+$("#start-btn").on("click", function () {
+  $(".hiddenShow").show()
+  $("#start-btn").hide()
 });
 
-// function for displaying questions
-var questionDisplay = function() {
-$(".questions :not('#sub-but')").empty();
-// loops through the 10 questions 
-for (var j = 0; j < 10; j++) {
-$('.questions').prepend('<div class="' + questions[j].name + '"></div>');
-$(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
-// loops through answers for each radio button
-for (var i = 0; i <= 3; i++) {
-    $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
+
+var index = 0
+$("#questions").text(questions[index].ques)
+$("#questions").append("<hr>"+questions[index].ans[0]+"<button class='answer-button' info='"+questions[index].ans[0]+"'>select</button>")
+$("#questions").append("<hr>"+questions[index].ans[1]+"<button class='answer-button' info='"+questions[index].ans[1]+"'>select</button>")
+$("#questions").append("<hr>"+questions[index].ans[2]+"<button class='answer-button' info='"+questions[index].ans[2]+"'>select</button>")
+$("#questions").append("<hr>"+questions[index].ans[3]+"<button class='answer-button' info='"+questions[index].ans[3]+"'>select</button>")
+
+
+$(document).on("click", ".answer-button", function() {
+var buttonClicked = $(this).attr("info")
+console.log(buttonClicked)
+
+if (buttonClicked == questions[index].correct) {
+alert("you are right")
+correctAnswers++
 }
-$('.questions').prepend('<hr />');
+
+else {
+  alert("you are wrong")
+  wrongAnswers++
 }
+
+index++
+if (index < 6) {
+  
+
+
+$("#questions").empty ()
+$("#questions").text(questions[index].ques)
+$("#questions").append("<hr>"+questions[index].ans[0]+"<button class='answer-button' info='"+questions[index].ans[0]+"'>select</button>")
+$("#questions").append("<hr>"+questions[index].ans[1]+"<button class='answer-button' info='"+questions[index].ans[1]+"'>select</button>")
+$("#questions").append("<hr>"+questions[index].ans[2]+"<button class='answer-button' info='"+questions[index].ans[2]+"'>select</button>")
+$("#questions").append("<hr>"+questions[index].ans[3]+"<button class='answer-button' info='"+questions[index].ans[3]+"'>select</button>")
+
 }
+else {
+  if (index == questions.length) {
+   console.log("thishappens")
+  $(".hiddenShow").hide()  
+ }
 
-
-// function for countdown timer
-var countdown = function(seconds) {
-
-var timer = setInterval(function() {
-seconds = seconds - 1;
-$("#time-remain").html(seconds);
-
-if (seconds <= 0) {
-    $('.container').fadeOut(500);
-    var correctAnswers = 0;
-    var wrongAnswers = 0;
-    var unAnswered = 0;
-
-    // loop through correctArray & radioName to match html elements & answers
-    for (var i = 0; i < 10; i++) {
-
-        if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
-
-            correctAnswers++;
-            console.log("this is correct! number:" + i)
-        } else {
-            wrongAnswers++;
-            console.log("this is wrong! number:" + i)
-        };
-    }
-    $('#correctTimesUp').append(correctAnswers);
-    // display wrongAnswers
-    $('#wrongTimesUp').append(wrongAnswers);
-    $('#timesUp').fadeIn(1000).show();
-
-    // alert("Times Up!");
-    clearInterval(timer);
-    return;
 }
-}, 1000);
+$("#wrong").text(wrongAnswers)
+$("#correct").text(correctAnswers) 
+ 
 
-// click event for submit button to stop timer
-$('#sub-but').on('click', function() {
-clearInterval(timer);
+
 })
-}; // end countdown
+
+var timeleft = 10;
+  var downloadTimer = setInterval(function(){
+  document.getElementById("progressBar").value = 10 - --timeleft;
+  if(timeleft <= 0)
+  $(".hiddenShow").hide()  
 
 
-// function to grade quiz once submit button is clicked
-var gradeQuiz = $('#sub-but').on('click', function() {
-
-var correctAnswers = 0;
-var wrongAnswers = 0;
-var unAnswered = 0;
-
-// loop through correctArray & radioName to match html elements & answers
-for (var i = 0; i < 10; i++) {
-
-if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
-
-    correctAnswers++;
-} else {
-    wrongAnswers++;
-};
-};
-
-// once submit is clicked...
-// tests
-// stop timer
-countdown();
-// fade out questions
-$('.container').fadeOut(500);
-// show answerScreen
-$('#answerScreen').show();
-// display correctAnswers
-$('#correctScreen').append(correctAnswers);
-// display wrongAnswers
-$('#wrongScreen').append(wrongAnswers);
-
-}); // end gradeQuiz
+},3000); 
